fe296c7553d7530a423fa38a0f786ecd6b4e3822
Sarp Gökdağ <sarpgo@icloud.com>
2018-04-09 21:25:37 -0400

    Update md5.go

--- md5.go
+++ md5.go
@@ -1,32 +1,14 @@
-/*
-Package Deep Recurrent Neural Networks and Long Short Term Memory in Golang.
-*/
 package main
 
 import (
-	"fmt"
+  "fmt"
+  "io"
+  "crypto/md5"
 )
 
-func hell() {
-	fmt.Println("test")
-}
-
-func main() {
-	/*var graph Graph = Graph{true, []func(){hell, hell}}
-
-	for i := range graph.Backprop {
-		graph.Backprop[i]()
-	}*/
-
-	mat := RandomMatrix(4, 4, 1, 1)
-	PrintMatrix(mat)
-	var graph1 Graph
-	PrintMatrix(graph1.Tanh(mat))
-	pluck := graph1.RowPluck(mat, 1)
-	PrintMatrix(pluck)
-
-	var a int = 0.0
-	var b int32 = a
-
-	print(b)
+func main(){
+  h := md5.New()
+  io.WriteString(h, " mesaj ")
+  fmt.Printf("%X", h.Sum(nil))
+  fmt.Printf("\n")
 }
