2494e7358194cde012df5320a56714e49c132a30
Sarp Gökdağ <sarpgo@icloud.com>
2018-04-09 21:15:23 -0400

    Add files via upload

---
+++ md5.go
@@ -0,0 +1,32 @@
+/*
+Package Deep Recurrent Neural Networks and Long Short Term Memory in Golang.
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func hell() {
+	fmt.Println("test")
+}
+
+func main() {
+	/*var graph Graph = Graph{true, []func(){hell, hell}}
+
+	for i := range graph.Backprop {
+		graph.Backprop[i]()
+	}*/
+
+	mat := RandomMatrix(4, 4, 1, 1)
+	PrintMatrix(mat)
+	var graph1 Graph
+	PrintMatrix(graph1.Tanh(mat))
+	pluck := graph1.RowPluck(mat, 1)
+	PrintMatrix(pluck)
+
+	var a int = 0.0
+	var b int32 = a
+
+	print(b)
+}
